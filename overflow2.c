a03169dfa39f8882a556deb8f34a0b865b384ba9
jkovba <jkovba@gmail.com>
2020-03-06 12:18:19 -0500

    Create overflow2.c

---
+++ overflow2.c
@@ -0,0 +1,15 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  gets(user_password); // get password from user
+
+  return 0;
+}
+
+int overflow()
+{
+  printf("You've earned an A+ for the class.")
+}
